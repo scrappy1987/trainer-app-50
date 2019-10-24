79a4f05bebaa0f33c33e9ab7f8db61ddddb5cbec
scrappy1987 <john.gordon@qa.com>
2019-10-24 14:57:15 +0100

    Added service component

--- src/main/java/com/qa/business/TrainerService.java
+++ src/main/java/com/qa/business/TrainerService.java
@@ -1,5 +1,26 @@
 package com.qa.business;
 
+import org.springframework.stereotype.Service;
+
+import com.qa.persistence.domain.Trainer;
+
+@Service
 public class TrainerService {
 
+	public Trainer getAllTrainers() {
+
+		return new Trainer("Jim", "Gordon");
+	}
+
+	public Trainer addNewTrainer() {
+		return new Trainer("Jim", "Gordon");
+	}
+
+	public Trainer updateTrainer() {
+		return new Trainer("Jim", "Gordon");
+	}
+
+	public Trainer deleteTrainer() {
+		return new Trainer("Jim", "Gordon");
+	}
 }
--- src/main/java/com/qa/rest/TrainerController.java
+++ src/main/java/com/qa/rest/TrainerController.java
@@ -1,5 +1,6 @@
 package com.qa.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,30 +8,34 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.business.TrainerService;
 import com.qa.persistence.domain.Trainer;
 
 @RestController
 @RequestMapping("/trainerapp")
 public class TrainerController {
 	
+	@Autowired
+	private TrainerService service;
+	
 	@GetMapping(value = "trainer")
 	public Trainer getAllTrainers(){
 		
-		return new Trainer("John", "Gordon");
+		return service.getAllTrainers();
 	}
 	 
 	@PostMapping(value = "trainer")
 	public Trainer addNewTrainer() {
-		return new Trainer("John", "Gordon");
+		return service.addNewTrainer();
 	}
 	
 	@PutMapping(value = "trainer")
 	public Trainer updateTrainer() {
-		return new Trainer("John", "Gordon");
+		return service.updateTrainer();
 	}
 	
 	@DeleteMapping(value = "trainer")
 	public Trainer deleteTrainer() {
-		return new Trainer("John", "Gordon");
+		return service.deleteTrainer();
 	}
 }
